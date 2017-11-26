6831be5ab405261f6f02a8b5cb056d5aa7fea724
Michelle Ocampo <manneocampo@gmail.com>
2017-11-26 15:25:42 -0500

    added starter code to the files

---
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -0,0 +1,28 @@
+//==========================================
+//DEPENDENCIES 
+//We need to include the path package to get the correct file path for our html
+//==========================================
+var path = require("path");
+
+//===========================================
+//Routing
+//===========================================
+
+
+module.exports = function(app){
+	//HTML GET requests 
+	//Below the code handles when users "visit" a page 
+	//In each of the below cases the user is shown an HTML page of content
+	
+	//Basic route that sends the user first to the AJAX page
+	app.get("/", function(req, res){
+		res.sendFile(path.join(__dirname, "../public/home.html"));
+	});
+
+	//Route that sends the user to the survey page 
+	app.get("/survey", function(req, res){
+		res.sendFile(path.join(__dirname, "survey.html"));
+	});
+};
+
+
---
+++ server.js
@@ -0,0 +1,32 @@
+//Dependencies 
+//=========================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Sets up the Express App 
+//=========================================================
+var app = express();
+var PORT = process.env.PORT || 3000; 
+
+//Sets up the Express app to handle data parsing 
+//some students had to change to true for it to work --keep in mind
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//=========================================================
+//ROUTER 
+//The below points our server to a series of "route" files 
+//These routes give our server a "map" of how to respond when user's visit or request data from various URLs
+//=========================================================
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+//starts the server to begin listening 
+//=========================================================
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT" + PORT);
+});
+
