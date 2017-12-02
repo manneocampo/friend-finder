aefd36ac26049663cb08e5035435bd5154d31ef3
Michelle Ocampo <manneocampo@gmail.com>
2017-12-01 19:35:03 -0500

    added complete set of friend data with stock pics

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,37 +2,166 @@
 //=========================================================
 var friends = [
 	{
-		"name": 'Michelle', 
-		"photo": "lkjhgd", 
+		"name": 'Sheryl', 
+		"photo": "https://images.pexels.com/photos/324658/pexels-photo-324658.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 
 		"scores":	[
 			1,
 			2,
 			3,
 			4,
 			5,
-			1,
+			3,
 			2,
 			3,
-			4,
+			1,
 			5
 		]
 	}, 
 	{
 		"name": 'Bob', 
-		"photo": "lkjhgd", 
+		"photo": "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 
 		"scores":	[
 			5,
 			4,
-			3,
 			2,
+			2,
+			1,
+			5,
+			5,
+			3,
+			4,
+			1
+		]
+	},
+	{
+		"name": 'Jake', 
+		"photo": "https://images.pexels.com/photos/450212/pexels-photo-450212.jpeg?h=350&auto=compress&cs=tinysrgb", 
+		"scores":	[
+			5,
+			1,
+			1,
+			3,
 			1,
 			5,
 			4,
 			3,
 			2,
+			2
+		]
+	},
+	{
+		"name": 'Pablo', 
+		"photo": "https://images.pexels.com/photos/428341/pexels-photo-428341.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 
+		"scores":	[
+			5,
+			5,
+			5,
+			3,
+			4,
+			5,
+			5,
+			3,
+			4,
+			3
+		]
+	},
+	{
+		"name": 'Maria', 
+		"photo": "https://images.pexels.com/photos/371168/pexels-photo-371168.jpeg?h=350&auto=compress&cs=tinysrgb", 
+		"scores":	[
+			4,
+			4,
+			2,
+			3,
+			5,
+			3,
+			1,
+			4,
+			2,
+			2
+		]
+	},
+	{
+		"name": 'Jillian', 
+		"photo": "https://images.pexels.com/photos/274595/pexels-photo-274595.jpeg?h=350&auto=compress&cs=tinysrgb", 
+		"scores":	[
+			2,
+			3,
+			2,
+			2,
+			5,
+			1,
+			3,
+			2,
+			4,
+			4
+		]
+	},
+	{
+		"name": 'Andrew', 
+		"photo": "https://images.pexels.com/photos/544716/pexels-photo-544716.jpeg?w=940&h=650&auto=compress&cs=tinysrgb", 
+		"scores":	[
+			3,
+			3,
+			1,
+			1,
+			1,
+			5,
+			5,
+			2,
+			3,
+			3
+		]
+	},
+	{
+		"name": 'ELizabeth', 
+		"photo": "https://images.pexels.com/photos/41272/clerk-business-businesswoman-busy-41272.jpeg?h=350&auto=compress&cs=tinysrgb", 
+		"scores":	[
+			1,
+			1,
+			2,
+			2,
+			2,
+			4,
+			4,
+			3,
+			4,
+			3
+		]
+	},
+	{
+		"name": 'Jane', 
+		"photo": "https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?h=350&auto=compress&cs=tinysrgb", 
+		"scores":	[
+			5,
+			4,
+			4,
+			5,
+			1,
+			1,
+			3,
+			3,
+			2,
+			5
+		]
+	},
+	{
+		"name": 'Tim', 
+		"photo": "https://images.pexels.com/photos/428339/pexels-photo-428339.jpeg?h=350&auto=compress&cs=tinysrgb", 
+		"scores":	[
+			4,
+			4,
+			3,
+			3,
+			2,
+			5,
+			5,
+			1,
+			4,
 			1
 		]
 	}
+
 ];
 
 module.exports = friends;
